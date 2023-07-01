ebbf3b80d5cdf0380eff45a615a467de4252e61d
Starcea <stardev.uwu@gmail.com>
2023-07-01 18:16:45 +0900

    refactor: type annotation

--- src/cache.ts
+++ src/cache.ts
@@ -3,7 +3,7 @@ import sha256 from 'crypto-js/sha256'
 import { appendFile, readFile } from 'fs/promises'
 import { join } from 'path'
 
-export const isCached = async (data: object): Promise<boolean> => {
+export const isCached = async (data: object) => {
   const hash = sha256(JSON.stringify(data)).toString()
 
   return readFile(join(__dirname, `../cache/${hash}.svg`))
@@ -11,17 +11,15 @@ export const isCached = async (data: object): Promise<boolean> => {
     .catch(() => false)
 }
 
-export const cache = async (data: object, svg: string): Promise<boolean> => {
+export const cache = async (data: object, svg: string) => {
   const hash = sha256(JSON.stringify(data)).toString()
 
   app.log.info(`Caching ${hash}`)
 
   await appendFile(join(__dirname, `../cache/${hash}.svg`), svg)
-
-  return true
 }
 
-export const getCache = async (data: object): Promise<string | null> => {
+export const getCache = async (data: object) => {
   const hash = sha256(JSON.stringify(data)).toString()
 
   app.log.info(`Using cache ${hash}`)
